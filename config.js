3258488a77d3ee2fdd35261658824853b413910b
KuzaKeenTV <78216489+KuzaKeenTV@users.noreply.github.com>
2024-02-16 06:50:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 module.exports = {
-  token: process.env.token || "MTEwNjg5OTEwMjUyMTkwNTIxNA.GGsWpF.HieuB2W_vhbFK4tXxk7VTtse-0x0PueY82BljU",
+  token: process.env.token,
   embedColor: "#f4942c",
   adminRole: ["864451453493444608", "765853320241217553", "765853320241217555"],
   adminUser: "311471678112661504",
   verifiedUser: ["933996717639077909"],
   GuildLeaderRoleID: "1140976442562318356",
   prefix: ">",
-  SPREADSHEET_ID: process.env.SPREADSHEET_ID || "1vHVL5TwYY5wcgpdFvd5EGnRvtr65g7ZzAJ5VzZY4DkA",
+  SPREADSHEET_ID: process.env.SPREADSHEET_ID,
 };
